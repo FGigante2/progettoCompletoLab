932d7ed8e8b110163dbe3118397df8d8f8d030c6
FGigante2 <139179362+FGigante2@users.noreply.github.com>
2023-07-12 15:51:37 +0200

    Update client1.c

--- client1.c
+++ client1.c
@@ -14,10 +14,8 @@
 
 // host e port a cui connettersi
 #define HOST "127.0.0.1"
-#define PORT 5050
+#define PORT 53563
 #define Max_sequence_length 2048
-
-atomic_int sequenze_inviate = 0;
 
 //funzioni readn e writen per leggere o scrivere nel socket
 ssize_t readn(int fd, void *ptr, size_t n) {  
@@ -51,42 +49,7 @@ size_t writen(int fd, void *ptr, size_t n) {
    return(n - nleft); /* return >= 0 */
 }
 
-
-void* terminazione_conn(){
-
- int inet_socket;
-    inet_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(inet_socket < 0){
-        perror("Errore nella creazione del socket");
-        exit(1);
-    };
-
-    //specifico l'indirizzo e la porta del socket
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = inet_addr(HOST);
-
-    //mi connetto al socket 
-    int stato_connessione = connect(inet_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-    if(stato_connessione == -1){
-        perror("C'\u00e8 stato un errore nella connessione con il socket");
-        exit(1);
-    }
-
-    //mando il carattere "C" che specifica il tipo di connessione
-    char word = 'C';
-    writen(inet_socket, &word, 1);
-
-    //mando le linee inviate, devono coincidere nel server
-    int seq_inv = htonl(sequenze_inviate);
-    writen(inet_socket, &seq_inv, 4);
-
-    return NULL;
-
-}
-
-int main(int argc, char *argv[]){   
+int main(int argc, char *argv[]){
 
     if(argc != 2){
         printf("Client1 deve ricevere un file da linea di comando, usare : 'client1 nomefile' ");
@@ -128,18 +91,16 @@ int main(int argc, char *argv[]){
         perror("C'\u00e8 stato un errore nella connessione con il socket");
         exit(1);
     }
-    printf("[CONNESSO] Connessione effettuata con il socket di indirizzo %d \n" , server_address.sin_addr.s_addr);
 
     //mando il carattere "A" che specifica il tipo di connessione
-    char word = 'A';
-    writen(inet_socket, &word, 1);
+    char tipo_connessione = 'A';
+    writen(inet_socket, &tipo_connessione, 1);
 
     //controllo che la linea non superi la lunghezza massima
     assert(strlen(linea) <= Max_sequence_length);
 
     // mandiamo la lunghezza della prossima linea
     short dim = strlen(linea);
-    printf("%d",dim);
     short tmp = htons(dim);
     writen(inet_socket, &tmp, 2);
 
@@ -147,11 +108,6 @@ int main(int argc, char *argv[]){
     char buffer2[dim];
     strcpy(buffer2 , linea);
     writen(inet_socket, &buffer2, dim);
-    printf("byte inviati %ld", sizeof(buffer2));
-    sequenze_inviate++;
-
-  
-
     close(inet_socket);
     }
 
